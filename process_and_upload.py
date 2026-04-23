79252747b64077e4cc79ba6ef4592a8efbf9f9dd
gamesidepadova-sys <gameside.padova@gmail.com>
2026-04-23 10:31:13 +0200

    Update process_and_upload.py

--- process_and_upload.py
+++ process_and_upload.py
@@ -22,7 +22,7 @@ ALLOWED_CAT1 = {
     "consumabili e ufficio",
     "salute, beauty e fitness",
 }
-EXCLUDE_TITLE_SUBSTRINGS = {"phs-memory", "montatura", "BlueOptics"}
+EXCLUDE_TITLE_SUBSTRINGS = {"phs-memory", "montatura", "blueoptics"}
 MIN_QTY = 10
 MAX_DIFF_0373 = 20
 
